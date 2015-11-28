a73b57f447c20ba5ffc30dc7c5b4d38f0a7d81c6
Łukasz Mróz <l.mroz88@gmail.com>
2015-11-28 11:43:37 +0100

    Struktura appki

--- www/modules/app.js
+++ www/modules/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
@@ -28,15 +28,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
             .state('app', {
                 url: '/app',
                 abstract: true,
-                templateUrl: 'views/global/menu.html',
+                templateUrl: 'modules/global/menu.html',
                 controller: 'AppCtrl'
             })
 
             .state('app.welcome', {
-                url: '/welcome',
+                url: '/wel',
                 views: {
                     'menuContent': {
-                        templateUrl: 'views/welcome/welcome.html',
+                        templateUrl: 'modules/welcome/welcome.html',
                         controller: 'WelcomeController',
                         controllerAs: 'welcome'
                     }
@@ -44,10 +44,10 @@ angular.module('starter', ['ionic', 'starter.controllers'])
             })
 
             .state('app.settings', {
-                url: '/settings',
+                url: '/set',
                 views: {
                     'menuContent': {
-                        templateUrl: 'views/settings/settings.html',
+                        templateUrl: 'modules/settings/settings.html',
                         controller: 'SettingsController',
                         controllerAs: 'settings'
                     }
@@ -91,5 +91,48 @@ angular.module('starter', ['ionic', 'starter.controllers'])
                 }
             });*/
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/app/welcome');
+        $urlRouterProvider.otherwise('/app/wel');
+    })
+
+  .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    // Form data for the login modal
+    $scope.loginData = {};
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('modules/login/login.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
     });
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+      $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function() {
+      $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+      console.log('Doing login', $scope.loginData);
+
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function() {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  })
+;
+
