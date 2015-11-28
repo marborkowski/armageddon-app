3ecff86c11feb6281ab20a9b77cb2e1b3f338bd5
Kamil Kuliczkowski <coolicz@gmail.com>
2015-11-28 13:50:55 +0100

    Map dev + added ionic.utils -> $localStorage

--- www/modules/map/map.service.js
+++ www/modules/map/map.service.js
@@ -1,3 +1,42 @@
-/**
- * Created by coolicz on 2015-11-28.
- */
+angular.module('starter')
+    .factory('MapData', ['$http', '$q', '$localStorage', function ($http, $q, $localStorage) {
+
+        var dummyMarkers = [
+            {
+
+            }
+        ];
+
+        var getOnlineMarkers = function() {
+
+        };
+
+        var getLocalMarkers = function() {
+
+        };
+
+        var getMarkers = function() {
+            var deferred = $q.defer();
+
+
+
+            return deferred.promise;
+        };
+
+        var saveMarkerOnline = function() {
+
+        };
+
+        var saveMarkerLocally = function() {
+
+        };
+
+        var saveMarker = function() {
+            $localStorage.set('test', 'abc');
+        };
+
+        return {
+            getMarkers: getMarkers,
+            saveMarker: saveMarker
+        };
+    }]);
